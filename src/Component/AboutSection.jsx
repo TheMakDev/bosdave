abb135fff09051d93863df70e703e24678d61167
TheMakDev <makanjuolaenitan71@gmail.com>
2025-06-05 10:18:48 +0100

    about

--- src/Component/AboutSection.jsx
+++ src/Component/AboutSection.jsx
@@ -47,7 +47,7 @@ const AboutSection = () => {
             “BOSDAVE ENGINEERING COMPANY LIMITED was established in 2002 and registered with the Companies and Allied Matters Act 1990 of Corporate Affairs Commission as DAVE ELECTRICAL SYSTEM which later metamorphosed to BOSDAVE ENGINEERING COMPANY LIMITED. ”
           </p>
           <p className="text-gray-500 text-sm sm:text-base leading-relaxed mb-6">
-            TThe company's corporate head office is located in the nation's capital, Abuja. The organization is a general contractor, offering services in various sectors like, Construction, Civil, Cleaning, Borehole Drilling and General Supply, etc. The company is a human resource nub where contractors and job handlers with maximum guarantee can shop for and access Engineers, skill workers, artisan and unskilled manual labourers depending on customers's construction needs.
+            The company's corporate head office is located in the nation's capital, Abuja. The organization is a general contractor, offering services in various sectors like, Construction, Civil, Cleaning, Borehole Drilling and General Supply, etc. The company is a human resource nub where contractors and job handlers with maximum guarantee can shop for and access Engineers, skill workers, artisan and unskilled manual labourers depending on customers's construction needs.
           </p>
           <button className="bg-orange-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition duration-300">
             ABOUT US
